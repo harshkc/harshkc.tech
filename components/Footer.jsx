42b397975de4554904025cac86fe3b5b69e126bb
harshkc <harsh.hkc1006@gmail.com>
2021-11-29 16:52:07 +0530

    adds header and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,110 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import {FaTwitter, FaLinkedinIn, FaGithub} from 'react-icons/fa';
+import {contactLinks, navItems} from '../util';
+
+export const socialIcons = [
+  {
+    name: 'twitter',
+    to: 'https://twitter.com/harshkc99',
+    icon: <FaTwitter />,
+    color: 'text-blue-400',
+  },
+  {
+    name: 'github',
+    to: 'https://github.com/harshkc',
+    icon: <FaGithub />,
+    color: 'text-gray-800',
+  },
+  {
+    name: 'linkedin',
+    to: 'https://www.linkedin.com/in/harshkc99',
+    icon: <FaLinkedinIn />,
+    color: 'text-blue-700',
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer
+      id="contact"
+      className="relative pt-20 mt-0 bg-gray-100 dark:bg-secondary "
+    >
+      <div className="container px-4 mx-0 sm:mx-auto">
+        <div className="flex flex-wrap text-left lg:text-left">
+          <div className="w-full px-4 lg:w-6/12">
+            <p className="pb-4 text-3xl font-semibold text-gray-700 dark:text-white">
+              Let's keep in touch!
+            </p>
+            <h5 className="mt-0 mb-2 text-lg text-gray-600 dark:text-gray-200">
+              Find Harsh on any of these platforms.
+            </h5>
+            <div className="flex my-6 row lg:mb-0">
+              {socialIcons.map(icon => (
+                <Link key={icon.name} href={icon.to}>
+                  <a>
+                    <button
+                      className={`transition duration-500 ease transform hover:-translate-y-1 bg-white dark:bg-primary cursor-pointer ${icon.color} dark:text-white shadow-lg font-normal h-10 w-10 flex items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2`}
+                      type="button"
+                    >
+                      {icon.icon}
+                    </button>
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="w-full px-4 my-2 lg:w-6/12">
+            <div className="flex flex-wrap mb-6 items-top">
+              <div className="w-full px-4 ml-auto lg:w-4/12">
+                <span className="block mb-2 text-sm font-semibold text-gray-500 uppercase dark:text-white">
+                  Contact
+                </span>
+                <ul className="list-unstyled">
+                  {contactLinks.map(contact => (
+                    <li
+                      key={contact.name}
+                      className="block pb-2 text-sm font-semibold text-gray-600 dark:text-gray-300 dark:hover:text-white hover:text-gray-800"
+                    >
+                      <Link href={contact.to}>
+                        <a>{contact.name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="w-full px-4 mt-2 sm:mt-0 lg:w-4/12">
+                <span className="block mb-2 text-sm font-semibold text-gray-500 uppercase dark:text-white">
+                  Site Map
+                </span>
+                <ul className="list-unstyled">
+                  {navItems.map(link => (
+                    <li
+                      key={link.name}
+                      className="block pb-2 text-sm font-semibold text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white"
+                    >
+                      <Link href={link.to}>
+                        <a>{link.name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-center pb-6 mt-4 md:justify-between">
+          <div className="w-full px-4 mx-auto text-center md:w-4/12">
+            <div className="py-1 text-sm font-medium text-gray-600 dark:text-white">
+              All rights reserved © Harsh Choudhary{' '}
+              <span id="get-current-year">{new Date().getFullYear()}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React from 'react';
+
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import {FaSun, FaMoon, FaBars} from 'react-icons/fa';
+import {navItems} from '../util';
+import {useTheme} from 'next-themes';
 
 const Header = () => {
-  return <div>Header</div>;
+  const router = useRouter();
+  const {theme, setTheme} = useTheme();
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <nav
+      id="header"
+      className="relative flex flex-wrap items-center justify-center px-2 py-3 mx-6 bg-white md:mb-3 dark:bg-primary"
+    >
+      <div className="container flex flex-wrap items-center justify-between py-8">
+        <div
+          className={`w-full relative whitespace-nowrap flex justify-between items-center md:w-auto md:static md:block md:justify-start font-medium text-2xl`}
+        >
+          <Link href="/">
+            <a>Harsh Choudhary</a>
+          </Link>
+          <div className="flex justify-end">
+            <button
+              className="block p-3 text-xl leading-none border border-solid rounded-full outline-none cursor-pointer border-gray md:hidden focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <FaBars />
+            </button>
+            <button
+              aria-label="Toggle theme"
+              onClick={() => toggleTheme()}
+              className="inline-flex items-center justify-center pl-3 md:hidden"
+            >
+              {process.browser ? (
+                theme === 'dark' ? (
+                  <FaMoon size="1.15em" />
+                ) : (
+                  <FaSun size="1.15em" />
+                )
+              ) : null}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`md:flex flex-grow  items-center ${
+            navbarOpen ? ' flex flex-start py-4 ' : ' hidden justify-center'
+          }`}
+          id="example-navbar-danger"
+        >
+          <ul className="flex flex-col list-none md:flex-row">
+            {navItems.map(link => (
+              <li
+                key={link.name}
+                className={`nav-item ${navbarOpen ? 'py-3' : ''}`}
+                onClick={navbarOpen ? () => setNavbarOpen(false) : null}
+              >
+                <Link href={link.to}>
+                  <a>
+                    <span
+                      className={`${!navbarOpen ? 'mx-6' : ''} ${
+                        router.pathname === link.to
+                          ? 'text-pink-600'
+                          : 'text-gray-600 dark:text-gray-200 dark:hover:text-pink-600'
+                      } font-medium cursor-pointer hover:text-pink-600 `}
+                    >
+                      {link.name}
+                    </span>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          aria-label="Toggle theme"
+          onClick={() => toggleTheme()}
+          className="hidden cursor-pointer md:block"
+        >
+          {process.browser ? (
+            theme === 'dark' ? (
+              <FaMoon size="1.35em" />
+            ) : (
+              <FaSun size="1.35em" />
+            )
+          ) : null}
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
