9208a59a69449f8f1330fd96562cd89b3f42bbec
harshkc <harsh.hkc1006@gmail.com>
2021-11-29 19:04:00 +0530

    adds projects section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import {LandingPage} from "../sections";
-import {Separator,SectionLayout} from "../components";
+import {Separator, SectionLayout, Projects} from "../components";
 
 export default function Home() {
   return (
@@ -8,8 +8,12 @@ export default function Home() {
         <LandingPage />
       </section>
       <Separator />
-      <SectionLayout id="about" spacing="pt-8 pb-2" name="About">
+      <SectionLayout id='about' spacing='pt-8 pb-2' name='About'>
         <Introduction />
+      </SectionLayout>
+      <Separator />
+      <SectionLayout id='projects' spacing='py-4' name='Projects'>
+        <Projects />
       </SectionLayout>
       <Separator />
     </>
