22a22f1b50e5782e03898ac9ef2b147102dbf0ff
Harsh Choudhary <harsh.hkc1006@gmail.com>
2021-11-29 17:09:13 +0530

    🎨  UI: adds introduction section (#5)
    * adds about section
    
    * minor fixes

---
+++ sections/Introduction.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import {CircleButton} from '../components';
+
+function Introduction() {
+  return (
+    <div className="relative grid grid-cols-4 pt-16 pb-4 mx-auto gap-x-4 md:grid-cols-8 lg:gap-x-6 lg:grid-cols-12 max-w-7xl">
+      <div className="container relative mx-auto w-60 h-60 sm:w-96 sm:h-96 col-span-full lg:col-span-4">
+        <Image
+          unoptimized
+          layout="fill"
+          src="/images/harsh_intro.jpeg"
+          alt="Harsh Kumar Chaudhary"
+        />
+      </div>
+      <div className="mt-12 col-span-full lg:col-span-6 lg:col-start-6 lg:mt-0">
+        <div className="text-xl leading-tight md:text-2xl">
+          Hi, I'm Harsh Choudhary. I want to make the world better place through
+          innovative and quality software.
+        </div>
+        <p className="mt-12 font-medium text-gray-400 text-large md:text-xl">
+          {`
+              I'm a Front-end and mobile developer with a passion for building scalable and maintainable applications.
+              I love to code in
+        `}
+          <span className="text-green-600">
+            JavaScript, TypeScript, React, Flutter, Dart, and C++.
+          </span>
+        </p>
+        <p className="mt-12 font-medium text-gray-400 text-large md:text-xl">
+          {`
+            I'm also a big sports enthusiast. When I'm not at the computer you can find me playing cricket,
+            watching football, or playing badminton. I'm also a big fan of the latest tech and gadgets.
+          `}
+        </p>
+        <Link href="https://drive.google.com/uc?id=145uHfu63jTnhRwrQxSJj3gnjCs-zz-XQ&export=download">
+          <a>
+            <button
+              type="button"
+              className="flex px-4 py-3 mt-6 font-medium text-white transition-transform duration-500 transform bg-green-600 rounded-full shadow-lg outline-none ease hover:translate-y-1 cursor pointer"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                />
+              </svg>
+              <span className="ml-2">Resume</span>
+            </button>
+          </a>
+        </Link>
+
+        <CircleButton to="#projects" text="Explore Projects" styles="mt-8" />
+      </div>
+    </div>
+  );
+}
+
+export default Introduction;
--- sections/index.jsx
+++ sections/index.jsx
@@ -1,2 +1,2 @@
 export {default as LandingPage} from './LandingPage';
-
+export {default as Introduction} from './Introduction';
