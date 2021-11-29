fe12b9b5732627557ffeaca5cbfefba13cf95247
Harsh Choudhary <harsh.hkc1006@gmail.com>
2021-11-29 13:22:15 +0530

    🔨 configs: tailwind, dependencies and public assets (#1)
    * configures dependencies and fonts
    
    * adds public assets and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './sections/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+      //these colors are for only dark mode
+      colors: {
+        primary: '#1f2028',
+        secondary: '#2e3039',
+      },
+      //for representing thick underline under links
+      boxShadow: {
+        link: '0 -4px 0 0 rgba(255, 216, 95, 0.7) inset',
+        hover: '0 -3rem 0 0 rgba(255, 216, 95, 0.7) inset',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
