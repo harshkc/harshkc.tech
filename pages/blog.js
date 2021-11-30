7587af3b1bf11d9cdf77a0314272ca80f6e50d34
harshkc <harsh.hkc1006@gmail.com>
2021-11-30 12:49:15 +0530

    adds blog routes

---
+++ pages/blog.js
@@ -0,0 +1,32 @@
+import {FeaturedPosts} from '../sections/index';
+import {PostCard, Categories, PostWidget} from '../components';
+import {getPosts} from '../services';
+
+export default function Home({posts}) {
+  return (
+    <>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts},
+  };
+}
