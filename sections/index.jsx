b8b8cb95be091efe63654d9d574f827261d87fa8
harshkc <harsh.hkc1006@gmail.com>
2021-11-30 12:49:40 +0530

    adds blog sections

--- sections/index.jsx
+++ sections/index.jsx
@@ -1,3 +1,6 @@
+export {default as AdjacentPosts} from "./AdjacentPosts";
+export {default as FeaturedPosts} from "./FeaturedPosts";
 export {default as LandingPage} from "./LandingPage";
 export {default as Introduction} from "./Introduction";
 export {default as Projects} from "./Projects";
+export {default as Work} from "./Work";
